d65577441f54d3ef1d83e3765da981fa370de860
Jmestrelozano <jmestrelozano@gmail.com>
2024-12-19 17:20:53 -0500

    ✨ feat: Add main categories and subcategories with sizes and types
    - Added a list of main categories: tennis, buzos, chaquetas, and blusas & camisas.
    - Defined subcategories for each main category along with available sizes.
    - Included specific brands (Nike, Adidas, Levi's, Puma) with their respective sizes but no subcategories.

---
+++ src/app/data/categories.ts
@@ -0,0 +1,51 @@
+const mainCategories = ['tennis', 'buzos', 'chaquetas', 'blusas & camisas'];
+
+const subCategories = [
+  {
+    type: 'tennis',
+    sizes: [36, 37, 38, 39, 40, 41, 42, 43, 44],
+    subCategory: ['Tennis Casuales', 'Tennis Deportivos', 'Chunkys'],
+  },
+  {
+    type: 'buzos',
+    sizes: ['S', 'M', 'L', 'XL', 'XXL'],
+    subCategory: [
+      'Buzos Básicos',
+      'Buzos Oversized',
+      'Buzos Crop',
+      'Buzos Tejidos',
+    ],
+  },
+  {
+    type: 'chaquetas',
+    sizes: ['S', 'M', 'L', 'XL', 'XXL'],
+    subCategory: ['Bomber', 'De Mezclilla', 'Blazer'],
+  },
+  {
+    type: 'blusas & camisas',
+    sizes: ['XS', 'S', 'M', 'L', 'XL'],
+    subCategory: ['Blusas Elegantes', 'Camisas de Lino', 'Blusas Casuales'],
+  },
+  {
+    type: 'Nike',
+    sizes: [36, 37, 38, 39, 40, 41, 42, 43, 44],
+    subCategory: null,
+  },
+  {
+    type: 'Adidas',
+    sizes: [36, 37, 38, 39, 40, 41, 42, 43, 44],
+    subCategory: null,
+  },
+
+  {
+    type: "Levi's",
+    sizes: [36, 37, 38, 39, 40, 41, 42, 43, 44],
+    subCategory: null,
+  },
+
+  {
+    type: 'Puma',
+    sizes: [36, 37, 38, 39, 40, 41, 42, 43, 44],
+    subCategory: null,
+  },
+];
